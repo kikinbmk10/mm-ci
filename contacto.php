849194e01b267641d0e43d4ac44ec5b68c88c4f2
kikinbmk10 <kikinbmk10@gmail.com>
2018-02-15 18:40:05 -0600

    [ADD] contact page

--- contacto.php
+++ contacto.php
@@ -4,13 +4,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>UI Toolkit | Contacto</title>
+  <title>MMCITE 9 | Contact</title>
   <link rel="icon" type="image/png" href="https://dummyimage.com/64x64/000/fff" sizes="64x64">
   <link rel="icon" type="image/png" href="https://dummyimage.com/180x180/000/fff" sizes="180x180">
   <!-- [#TODO] Dont forget to update the FAVICON IMAGES in the next route -->
   <!-- <link rel="icon" type="image/png" href="assets/images/favicon-180x180.png" sizes="128x128"> -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" integrity="sha256-K3Njjl2oe0gjRteXwX01fQD5fkk9JFFBdUHy/h38ggY=" crossorigin="anonymous" rel="stylesheet">
   <link href="./assets/styles/main.css" rel="stylesheet">
+  
+  <link rel="stylesheet" type="text/css" href="assets/slick/slick.css"/>
+  <link rel="stylesheet" type="text/css" href="assets/slick/slick-theme.css"/>
 </head>
 
 <body>
@@ -103,131 +106,215 @@
       </div>
     </header>
   </div>
- 
+  <!--  Hero -->
+  <div class="hero-content-products">
+    <div class="contacto-slick">
+      <div class="hero-contact-item-1">
+        <h2 class="item-title">CONTACT</h2>
+      </div>
+      <div class="hero-contact-item-2">
+        <h2 class="item-title">CONTACT</h2>
+      </div>
+      <div class="hero-contact-item-3">
+        <h2 class="item-title">CONTACT</h2>
+      </div>
+      <div class="hero-contact-item-4">
+        <h2 class="item-title">CONTACT</h2>
+      </div>
+    </div>
+  </div>
   <!--  Main content -->
   <main class="j-workspace ">
     <div class="j-wrap">
-      <h2 class="margin-48">Contacto</h2>
-      <div class="main-contact-content">
-        <form id="contact-form"  action="#" method="POST" enctype="multipart/form-data">
-            <!-- Text input -->
-            <div class="input-container">
-              <label for="nombre">Nombre</label>
-              <input id="nombre" name="nombre" type="text">
+			<!-- section welcome -->
+			<div class="menu-a">
+        <h3><a href="index.html" class="inactive">HOME</a></h3>
+        <p>&nbsp;&nbsp;>>&nbsp;&nbsp;</p>
+        <h3><a href="#" class="active">CONTACT</a></h3>
+			</div>
+			<!-- contact section -->
+			<div class="contact">
+				<div class="accordion">
+          <div class="half-accordion">
+            <div class="tab blue">
+              <input id="tab-1" type="radio" name="tabs2" checked>
+              <label for="tab-1">Contact Address</label>
+              <div class="tab-content">
+                <h4>mmcité1 a.s (<a href="#">Map</a>)</h4>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>Bilovice 519</p>
+                    <p>687 12 Bilovice</p>
+                    <p>Czech Republic</p>
+                  </div>
+                </div>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>IC 276 70 864</p>
+                    <p>DIC CZ276 70 864</p>
+                    <p>registered in the Commercial Register kept at the</p>
+                    <p>Regional Court in Brno. under file number B4523</p>
+                  </div>
+                </div>
+                <h4 class="h4-second">mmcité + as (<a href="#">Map</a>)</h4>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>with its registered office at Bilovice 519</p>
+                    <p>687 12 Bilovice</p>
+                    <p>Czech Republic</p>
+                  </div>
+                </div>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>+420 572 434 292</p>
+                  </div>
+                </div>
+                <div class="info-content">
+                  <div class="info-icon">
+                    <img src="https://dummyimage.com/12x12/000/fff" alt="">
+                  </div>
+                  <div class="info-text">
+                    <p>info@mmciteplus.com</p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="input-container">
-            <!-- Text input -->
-              <label for="apellido">apellido</label>
-              <input id="apellido" name="apellido" type="text">
+            <div class="tab blue">
+              <input id="tab-2" type="radio" name="tabs2">
+              <label for="tab-2" class="top-label">Contact Persons -</label>
+              <label for="tab-2" class="sublabel">Urban furniture - domestic</label>
+              <div class="tab-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+              </div>
             </div>
-            <div class="input-container">
-            <!-- Email -->
-              <label>Correo</label>
-              <input type="email" name="correo">
+            <div class="tab blue">
+              <input id="tab-3" type="radio" name="tabs2">
+              <label for="tab-3" class="top-label">Contact Person -</label>
+              <label for="tab-3" class="sublabel">Urban furniture - export</label>
+              <div class="tab-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+              </div>
             </div>
-            <div class="input-container">
-            <!--  Telephone-->
-              <label>Telefono</label>
-              <input type="tel" name="telefono" id="telefono">
+            <div class="tab blue">
+              <input id="tab-4" type="radio" name="tabs2">
+              <label for="tab-4" class="top-label">Contact Persons mmcite +</label>
+              <label for="tab-4" class="sublabel">- Infrastructure projects</label>
+              <div class="tab-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+              </div>
             </div>
-<<<<<<< HEAD
-           
-=======
-            <div class="input-container">
-              <!-- Checkbox with label  -->
-              <label>
-                <input name="check" type="checkbox" value=""> <b>Checkbox input</b>
-              </label>
+            <div class="tab blue">
+              <input id="tab-5" type="radio" name="tabs2">
+              <label for="tab-5">Representation</label>
+              <div class="tab-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+              </div>
             </div>
->>>>>>> 0f908f587ca8774406807e9d6b98f4f55ba0dd8c
-            <div class="input-container">
-              <label>Select field</label>
-              <select name="opciones" id="form-select">
-                <option value="Option 03">Option 01</option>
-							  <option value="Option 03">Option 02</option>
-							  <option value="Option 03">Option 03</option>
-              </select>
-            </div>
-            <div class="input-container">
-              <label>Mensaje</label>
-						  <textarea cols="30" rows="5" placeholder="Escribe aqui..." name="mensaje"></textarea>
-            </div>
-						<button class="bg-color-success" type="submit">ENVIAR</button>
-        </form>
-        <div class="information-content">
-            <div class="p-img-left">
-                <img src="https://dummyimage.com/800x800/000/fff" alt="[#TODO]">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-						</div>
-						<div class="p-img-left">
-                <img src="https://dummyimage.com/800x800/000/fff" alt="[#TODO]">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-						</div>
-						<div class="p-img-left">
-                <img src="https://dummyimage.com/800x800/000/fff" alt="[#TODO]">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
+          </div>
+        </div>
+        <div class="contact-content">
+
         </div>
       </div>
-     
-    </div>
+		</div>
+		<!-- section banner -->
+		<section class="banner">
+			<div class="banner-content">
+				<h2>WOULD YOU LIKE TO RECEIVE INFORMATION ABOUT OUR NEWS?</h2>
+				<form class="banner-subscribe-content" action="#" method="POST">
+					<input type="email" name="bannerEmail" id="bannerEmail" placeholder="EMAIL ADDRESS">
+					<button type="submit">SUBSCRIBE</button>
+				</form>
+			</div>
+		</section>
   </main>
   <footer>
+		<div class="j-wrap">
+				<div class="footer-content-top">
+					<div class="footer-first-content">
+						<div class="footer-logos">
+							<img src="https://dummyimage.com/80x80/000/fff" alt="">
+							<img src="https://dummyimage.com/80x80/000/fff" alt="">
+						</div>
+						<p>Public realms are fascinating places where people meet each other and also experience the history of the city.</p>
+					</div>
+					<hr class="footer-hr-vertical">
+					<div class="footer-first-list">
+						<h3>INFORMATION</h3>
+						<a href="#">About us</a>
+						<a href="#">Careers</a>
+						<a href="#">Privacy & Security</a>
+						<a href="#">Site map</a>
+						<a href="#">Terms of use</a>
+						<a href="#">Blog</a>
+					</div>
+					<hr class="footer-hr-vertical">
+					<div class="footer-second-list">
+						<h3>HELP & SUPPORT</h3>
+						<a href="#">Recommended Reading</a>
+						<a href="#">Submit your Story</a>
+						<a href="#">Contact Us</a>
+						<a href="#">Frequently Asked Question</a>
+						<a href="#">How to Order</a>
+          </div>
+          <hr class="footer-hr-vertical">
+					<div class="footer-third-list">
+						<h3>MY ACCOUNT</h3>
+						<a href="#">Delivery Information</a>
+						<a href="#">My Account</a>
+						<a href="#">Order Tracking</a>
+						<a href="#">Reminder Service</a>
+						<a href="#">Shipping, Returns & Fees</a>
+					</div>
+				</div>
+    </div>
+    <hr class="footer-hr-horizontal">
+    <div class="j-wrap">
+      <div class="footer-content-bottom">
+        <p>Copyright © 2018, Mmcite 9. - All Rights Reserved</p>
+        <div class="social-networks">
+          <img src="https://dummyimage.com/48x48/000/fff" alt="">
+          <img src="https://dummyimage.com/48x48/000/fff" alt="">
+          <img src="https://dummyimage.com/48x48/000/fff" alt="">
+        </div>
+      </div>
+    </div>
   </footer>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="assets/scripts/ripple.js"></script>
-	<script src="assets/scripts/header.js"></script>
-  <!-- SweetAlert2 by kikin -->
-  <script src="assets/scripts/sweetalert2/dist/sweetalert2.all.min.js"></script>
-  <!-- Include a polyfill for ES6 Promises (optional) for IE11 and Android browser -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
-  <!-- validar formulario by kikin -->
-  <script src="assets/scripts/enviarForm.js"></script>
-	<script>
-		    $('#contact-form').submit(function(e){
-      	e.preventDefault();
-      	$('input').removeClass('error');
-			
-      	$('textarea').removeClass('error');
-      	$('label.error').remove();
-				var tel =$("input[name='telefono']");
-      	var name=$("input[name='nombre']");
-        var apellido=$("input[name='apellido']");
-      	var mensaje=$("textarea[name='mensaje']");
-      	var email=$("input[name='correo']");
-      	var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
-      	if(name.val() === ""){
-      		$(name).addClass('error');
-      		$(name).after('<label class="error"> Campo obligatorio</label>');
-      	}
-      	if(apellido.val() === ""){
-      		$(apellido).addClass('error');
-      		$(apellido).after('<label class="error"> Campo obligatorio</label>');
-				}
-      	if(mensaje.val() === ""){
-      		$(mensaje).addClass('error');
-      		$(mensaje).after('<label class="error"> Campo obligatorio</label>');
-      	}
-      	if(!pattern.test(email.val())){
-      		$(email).addClass('error');
-      		$(email).after('<label class="error"> El correo electrónico señalado es incorrecto</label>');	
-				} 
-      	if($('label.error').length == 0){
-
-          console.log("se enviara un correo");
-          contactoEnviarMensajeBtn_click();
-<<<<<<< HEAD
-          //contactoEnviarDatos();   DESCOMENTAR SOLO CUANDO SE TENGA EL CORREO DEL ADMIN DE LA PAGINA EN EL ARCHIVO send-data.php EN LA VARIABLE $admin
-
-          //estas funciones se encuentran en el script "enviarForm.js"
-=======
-          //contactoEnviarDatos();
->>>>>>> 0f908f587ca8774406807e9d6b98f4f55ba0dd8c
-      	}
-          return false;
+  <script src="assets/scripts/header.js"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script type="text/javascript" src="assets/slick/slick.min.js"></script>
+  <script type="text/javascript">
+      $(document).ready(function(){
+         $('.contacto-slick').slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'ease',
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1000,
+            arrows:false,
+            pauseOnHover: false,
+         });
       });
-	</script>
-
+ </script>
 </body>
 </html>
